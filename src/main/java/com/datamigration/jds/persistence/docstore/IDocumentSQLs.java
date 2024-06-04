91b46c454cd07d6d702650e07acfaa97855d1cc3
hdg <huy.duong@dm-international.com>
2024-06-04 16:20:15 +0200

    change: underline to minus

--- src/main/java/com/datamigration/jds/persistence/docstore/IDocumentSQLs.java
+++ src/main/java/com/datamigration/jds/persistence/docstore/IDocumentSQLs.java
@@ -3,98 +3,98 @@ package com.datamigration.jds.persistence.docstore;
 public interface IDocumentSQLs {
 
 	String CREATE_DOCSTORE_TABLE_SQL = """
-		IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='JIVS_DOCSTORE' and xtype='U')
-				CREATE TABLE jds.JIVS_DOCSTORE (
+		IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='[JIVS-DOCSTORE]' and xtype='U')
+				CREATE TABLE [jds].[JIVS-DOCSTORE] (
 				    ID            uniqueidentifier default newsequentialid() not null
-				        constraint JIVS_DOCSTORE_pk
+				        constraint [JIVS-DOCSTORE-pk]
 				        primary key,
-				 	FILE_BIN UNIQUEIDENTIFIER not null,
+				 	[FILE-BIN] UNIQUEIDENTIFIER not null,
 				 	FILENAME nvarchar(max) not null,
-				 	DOCUMENT_TYPE varchar(40) not null,
+				 	[DOCUMENT-TYPE] varchar(40) not null,
 				 	CREATOR UNIQUEIDENTIFIER not null,
 				    CREATED datetime not null,
-				    CUSTOMER_FK UNIQUEIDENTIFIER not null,
-				    SYSTEM_FK UNIQUEIDENTIFIER,
-				    CASE_ID UNIQUEIDENTIFIER,
+				    [CUSTOMER-FK] UNIQUEIDENTIFIER not null,
+				    [SYSTEM-FK] UNIQUEIDENTIFIER,
+				    [CASE-ID] UNIQUEIDENTIFIER,
 				    DELETED bit default 0 not null,
 				)
 		""";
 
 	String INSERT_DOCUMENT_SQL = """
-		INSERT INTO jds.JIVS_DOCSTORE(FILE_BIN, FILENAME, DOCUMENT_TYPE, CREATOR, CREATED, CUSTOMER_FK, SYSTEM_FK, CASE_ID, DELETED)
+		INSERT INTO [jds].[JIVS-DOCSTORE]([FILE-BIN], FILENAME, [DOCUMENT-TYPE], CREATOR, CREATED, [CUSTOMER-FK], [SYSTEM-FK], [CASE-ID], DELETED)
 		OUTPUT INSERTED.ID
 		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
 		""";
 
 	String SELECT_DOCUMENT_BY_ID = """
 		SELECT *
-		FROM jds.JIVS_DOCSTORE
+		FROM [jds].[JIVS-DOCSTORE]
 		WHERE ID = ?
 		""";
 
 	String SELECT_ALL_DOCUMENTS_SQL = """
 		SELECT *
-		FROM jds.JIVS_DOCSTORE
+		FROM [jds].[JIVS-DOCSTORE]
 		""";
 
 	String SELECT_DOCUMENT_BY_ID_SQL = """
 			SELECT *
-			FROM jds.JIVS_DOCSTORE
+			FROM [jds].[JIVS-DOCSTORE]
 			WHERE ID = ?
 			AND DELETED = 0
 		""";
 
 	String SELECT_DOCUMENT_BY_FILENAME_SQL = """
 			SELECT *
-			FROM jds.JIVS_DOCSTORE
+			FROM [jds].[JIVS-DOCSTORE]
 			WHERE FILENAME = ?
 			AND DELETED = 0
 		""";
 
 	String SELECT_DOCUMENT_BY_DOCUMENT_TYPE_SQL = """
 			SELECT *
-			FROM jds.JIVS_DOCSTORE
-			WHERE DOCUMENT_TYPE = ?
+			FROM [jds].[JIVS-DOCSTORE]
+			WHERE [DOCUMENT_TYPE] = ?
 			AND DELETED = 0
 		""";
 
 	String SELECT_DOCUMENT_BY_CREATOR_SQL = """
 			SELECT *
-			FROM jds.JIVS_DOCSTORE
+			FROM [jds].[JIVS-DOCSTORE]
 			WHERE CREATOR = ?
 			AND DELETED = 0
 		""";
 
 	String SELECT_DOCUMENT_BY_CREATED_SQL = """
 			SELECT *
-			FROM jds.JIVS_DOCSTORE
+			FROM [jds].[JIVS-DOCSTORE]
 			WHERE CREATED = ?
 			AND DELETED = 0
 		""";
 
 	String SELECT_DOCUMENT_BY_CUSTOMER_FK_SQL = """
 			SELECT *
-			FROM jds.JIVS_DOCSTORE
-			WHERE CUSTOMER_FK = ?
+			FROM [jds].[JIVS-DOCSTORE]
+			WHERE [CUSTOMER_FK] = ?
 			AND DELETED = 0
 		""";
 
 	String SELECT_DOCUMENT_BY_SYSTEM_FK_SQL = """
 			SELECT *
-			FROM jds.JIVS_DOCSTORE
-			WHERE SYSTEM_FK = ?
+			FROM [jds].[JIVS-DOCSTORE]
+			WHERE [SYSTEM_FKv = ?
 			AND DELETED = 0
 		""";
 
 	String SELECT_DOCUMENT_BY_CASE_ID_SQL = """
 			SELECT *
-			FROM jds.JIVS_DOCSTORE
-			WHERE CASE_ID = ?
+			FROM [jds].[JIVS-DOCSTORE]
+			WHERE [CASE_ID] = ?
 			AND DELETED = 0
 		""";
 
 	String UPDATE_DOCUMENT_DELETE_FLAG_SQL = """
-		UPDATE jds.JIVS_DOCSTORE
+		UPDATE [jds].[JIVS-DOCSTORE]
 		SET DELETED = ?
 		WHERE ID = ?
 		""";
--- src/test/java/com/datamigration/jds/util/ITestSQLs.java
+++ src/test/java/com/datamigration/jds/util/ITestSQLs.java
@@ -3,15 +3,15 @@ package com.datamigration.jds.util;
 public interface ITestSQLs {
 
 	String TRUNCATE_DB = """
-		IF EXISTS (SELECT 1 FROM sys.tables WHERE name = 'JIVS_DOCUMENT_STORE')
-			DELETE FROM jds.JIVS_DOCUMENT_STORE;
-		IF EXISTS (SELECT 1 FROM sys.tables WHERE name = 'JIVS_DOCUMENT_PARAM')
-			DELETE FROM jds.JIVS_DOCUMENT_PARAM;
+		IF EXISTS (SELECT 1 FROM sys.tables WHERE name = '[JIVS-DOCSTORE]')
+			DELETE FROM [jds].[JIVS-DOCSTORE];
+		IF EXISTS (SELECT 1 FROM sys.tables WHERE name = '[JIVS-DOCUMENT-PARAM]')
+			DELETE FROM [jds].[JIVS-DOCUMENT-PARAM];
 		""";
 
 	String SELECT_DOCUMENT_SQL = """
 		SELECT ID, FILENAME, CREATOR, DELETED
-		FROM jds.JIVS_DOCSTORE
+		FROM [jds].[JIVS-DOCSTORE]
 		WHERE ID = ?
 		""";
 }
